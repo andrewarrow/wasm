bde2c258df6701abf9061f76fa58aa00ffbbe806
Andrew Arrow <oneone@gmail.com>
2023-10-18 12:36:04 -0700

    The EXE circuit is down, navigate the open-source array so we can override the AGP feed!

--- web/markup/html.go
+++ web/markup/html.go
@@ -7,69 +7,57 @@ import (
 )
 
 type Tag struct {
-	Name string
-	//Children []*Tag
-	Parent *Tag
+	Name     string
+	Children []*Tag
+	//Parent *Tag
 }
 
-func NewTag(name string, parent *Tag) *Tag {
+func NewTag(name string) *Tag {
 	t := Tag{}
 	t.Name = name
-	//t.Children = []*Tag{}
-	t.Parent = parent
+	t.Children = []*Tag{}
+	//t.Parent = parent
 	return &t
 }
 
 func ToHTML(filename string) string {
-	all := []*Tag{}
 	buffer := []string{}
 
 	asBytes, _ := ioutil.ReadFile("markup/" + filename)
 	asString := string(asBytes)
 	asLines := strings.Split(asString, "\n")
-	var root *Tag
-	var current *Tag
+	root := NewTag("root")
+
+	stack := []*Tag{root}
 	var lastSpaces int
+
 	for _, line := range asLines {
 		tokens := strings.Split(line, " ")
+
 		spaces := countSpaces(tokens)
 		fmt.Println(spaces)
+		var tag *Tag
 		if spaces == 0 {
-			root = NewTag(tokens[0], nil)
-			current = root
+			tag = NewTag(tokens[0])
 		} else if spaces == 2 && lastSpaces == 0 {
-			current = NewTag(tokens[2], current)
-			all = append(all, current)
+			tag = NewTag(tokens[2])
 		} else if spaces == 4 && lastSpaces == 2 {
-			current = NewTag(tokens[4], current)
-			all = append(all, current)
+			tag = NewTag(tokens[4])
 		} else if spaces == 4 && lastSpaces == 6 {
-			current = current.Parent
-			current = NewTag(tokens[4], current)
-			all = append(all, current)
+			stack = stack[0 : len(stack)-1]
+			tag = NewTag(tokens[4])
 		} else if spaces == 6 && lastSpaces == 4 {
-			current = NewTag(tokens[6], current)
-			all = append(all, current)
+			tag = NewTag(tokens[6])
 		}
+
+		parent := stack[len(stack)-1]
+		parent.Children = append(parent.Children, tag)
+		stack = append(stack, tag)
 		lastSpaces = spaces
 	}
 
-	tags := findChildren(root, all)
-	fmt.Println(tags)
-
 	buffer = append(buffer, `<div>hi2</div>`)
 	return strings.Join(buffer, "\n")
-}
-
-func findChildren(root *Tag, all []*Tag) []*Tag {
-	some := []*Tag{}
-	for _, item := range all {
-		if item.Parent == root {
-			fmt.Println(item.Name)
-			some = append(some, item)
-		}
-	}
-	return some
 }
 
 func countSpaces(tokens []string) int {
